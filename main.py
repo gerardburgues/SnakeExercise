0cfcfcdb98f4630d86ec7d2c9407d6c265f2626b
GerardBurgues <gerard61999@gmail.com>
2022-06-18 19:59:16 +0200

    Clean. Need to do arguments

--- main.py
+++ main.py
@@ -1,72 +1,84 @@
 import numpy as np
 from itertools import combinations_with_replacement,combinations,permutations, product
 import copy
-
-
-
+import argparse
 
 def init_snake():
+    """
+    Initialize the grid and snake position
+    """
     init_array =  np.zeros(shape=(10,10))
     snake = [[5,5],[4,5],[4,4],[4,5]]
+
     #Setting snake
     for i in range(0,len(snake)):
         init_array[snake[i][0],snake[i][1]] = 1
     return init_array,snake
 
 def update_matrix(snake,matrix):   
+    """
+    matrix: Start position of the grid
+    snake: Cells where the snake appears
+    Updating matrix visualization
+    """
     for i in range(0,len(snake)):
         matrix[snake[i][0],snake[i][1]] = 1
     print(matrix)
 
-def case_of_error(snake, bold,matrix):
-    # If there is intersection with snake body then 
-    # count as false and jump interation
-    if snake[0] in snake[1:] or bold == False:
-        print("error here")
-        return False
-    else:
-        update_matrix(snake,matrix)
-        print("---------------_GOOD_----------------")
-        return True
 def possible_combinations(depth):
-    #Left Right Up Down
-    #Create all possible position for Left Right Up Down
+    """
+    depth: max number of combinations to consider 
+
+    This function creates all the possible combinations
+    for the snake to move.
+    """
     sample_list =  ['Left', 'Right', 'Up','Down']
-    list_combinations = list()
     combinations = [p for p in product(sample_list, repeat=depth)]
-    #combinations = list(permutations(sample_list, 3))
-    print(combinations)
-    #Let's create dictionary for validation of samples
-    dict_result = {}
-    for comb in combinations:
-        dict_result[comb] = False
-    return combinations,dict_result
-def move_snake(combinations, init_array, snake, dict_result):
+   
+    return combinations
+
+def move_snake(combinations, init_array, snake):
+    """
+    combinations: All possible combinations of movement
+    init_array: Start position of the snake
+    snake: Cells where the snake appears
+    
+    This function moves the snake and add the correct result
+    to a list. 
+    """
+
     cont = 0
     resultant_array = []
     for i in combinations:
-        print("This is combination: " , i)
         init_array, snake = init_snake()
-        if direction_snake(init_array, snake, i, dict_result) == True:
+        if direction_snake(init_array, snake, i) == True:
             cont +=1
             resultant_array.append(i)
 
-    print(cont)
-    print(resultant_array)
-def direction_snake(matrix, snake, comb, dict_result):
-    #Hem de tenir en consideració que quan j es negativa Error
-    #Moving head positions
-    print("Cmon!" ,snake)
-    #This variable is going to help us just run print 
-    #those matrix we are interested (No Error)
-    bold = True 
-    for i in comb:
-        print(i)
-        print("this is matrix")
-        print(matrix)
+    return cont, resultant_array
+    
+
+def direction_snake(matrix, snake, comb):
+    """
+    matrix: start position of the snake
+    snake: cells where the snake appears
+    comb: All possible combinations of movements
+    returns:
+        True if we can move the snake
+        False if we can't move the snake
+    """
+    
+    """
+    For each iteration we are going to do the movement.
+    Taking into account:
+        * overlapping of position => ERROR
+        * stepping out of the grid => ERROR
+    """
+
+    bold = True  #If its False means there is ERROR
+    for i in comb:  #Looping over movements combinations
         previous_pos = copy.copy(snake[0])
         matrix[snake[-1][0],snake[-1][1]] = 0    
-
         if i == 'Left':   
             snake[0][1]= snake[0][1]-1
             if snake[0][1] >= 0  :
@@ -75,13 +87,12 @@ def direction_snake(matrix, snake, comb, dict_result):
                     snake[i] = previous_pos
                     previous_pos = aux  
             else:
-                print("ERROR")
                 bold = False
                 break
             if snake[0] in snake[1:] :
-                print("ERROR")
                 bold = False
-                break   
+                break  
+
         if i == 'Right':     
             snake[0][1]= snake[0][1]+1
             varoa = init_array.shape[1]-1   
@@ -91,13 +102,12 @@ def direction_snake(matrix, snake, comb, dict_result):
                     snake[i] = previous_pos
                     previous_pos = aux
             else:
-                print("ERROR")
                 bold = False
                 break  
             if snake[0] in snake[1:] :
-                print("ERROR")
                 bold = False
                 break
+
         if i == 'Up': 
             snake[0][0]= snake[0][0]-1
             if snake[0][0] >= 0  :
@@ -106,13 +116,12 @@ def direction_snake(matrix, snake, comb, dict_result):
                     snake[i] = previous_pos
                     previous_pos = aux
             else:
-                print("ERROR")
                 bold = False
                 break 
             if snake[0] in snake[1:] :
-                print("ERROR")
                 bold = False
                 break  
+
         if i == 'Down':
             snake[0][0]= snake[0][0]+1
             print(snake[0][0])
@@ -124,19 +133,28 @@ def direction_snake(matrix, snake, comb, dict_result):
                     previous_pos = aux
                 print(snake)
             else:
-                print("ERROR")
                 bold = False
                 break
             if snake[0] in snake[1:] :
-                print("ERROR")
                 bold = False
                 break
-    if case_of_error(snake,bold,matrix) == True:
+
+    if bold == True:
+        update_matrix(snake,matrix)
         return True
 
 
 if __name__ == '__main__':
-    #initialize snake
+    """
+    Take into consideration position of matrix : [rows,columns]
+    For the test cases I had to change the position of i and j.
+    Before [columns, rows] 
+    Now [rows, columns]
+    """
+
+    #Start program
     init_array, snake = init_snake()
-    combinations,dict_result  = possible_combinations(4)
-    move_snake(combinations, init_array, snake, dict_result)
+    combinations  = possible_combinations(4)
+    cont, result = move_snake(combinations, init_array, snake)
+    print("We can find: ",  cont)
+    print("Those combinations are: ", result)
